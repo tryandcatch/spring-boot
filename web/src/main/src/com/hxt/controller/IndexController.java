0efe531b86ff700be9c15780f2d14714d7629a7c
nvlll <478956990@qq.com>
2017-06-27 17:30:03 +0800

    返回json

--- web/src/main/src/com/hxt/controller/IndexController.java
+++ web/src/main/src/com/hxt/controller/IndexController.java
@@ -1,5 +1,6 @@
 package com.hxt.controller;
 
+import com.hxt.controller.entity.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,16 +11,29 @@ import org.springframework.web.bind.annotation.ResponseBody;
  */
 @Controller
 public class IndexController {
+    /**
+     * 返回页面
+     * @param model
+     * @return
+     */
     @RequestMapping("/index")
     public String index(Model model){
         model.addAttribute("userName","hxt");
         return "index";
     }
 
+
+    /**
+     * 返回json
+     * @return
+     */
     @RequestMapping("/getJson")
     @ResponseBody
-    public String getJson(){
-
-        return "";
+    public User getJson(){
+        User user=new User();
+        user.setUserName("hxt");
+        user.setUserName("张三");
+        user.setPassWord("ssssss");
+        return user;
     }
 }
