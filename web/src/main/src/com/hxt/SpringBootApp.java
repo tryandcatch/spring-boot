306281428c9528811e10c5c193bb774a97d32228
nvlll <478956990@qq.com>
2017-06-27 14:55:25 +0800

    代码

---
+++ web/src/main/src/com/hxt/SpringBootApp.java
@@ -0,0 +1,14 @@
+package com.hxt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Huangxiutao on 2017/6/27.
+ */
+@SpringBootApplication
+public class SpringBootApp {
+    public static void main(String[] args){
+        SpringApplication.run(SpringBootApp.class,args);
+    }
+}
